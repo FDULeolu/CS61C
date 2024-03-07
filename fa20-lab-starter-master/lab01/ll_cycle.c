655b123298d15ad70097ecadc699a6bd98d1b16b
LeoLu <fdulyz@sina.com>
2024-03-07 14:23:32 +0800

    Lab01: submit my answer

--- fa20-lab-starter-master/lab01/ll_cycle.c
+++ fa20-lab-starter-master/lab01/ll_cycle.c
@@ -2,6 +2,23 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    if (head == NULL) {
+        return 0;
+    }
+    
+    node* fast;
+    node* slow;
+    fast = head;
+    slow = head;
+    do {
+        if (fast->next == NULL || fast->next->next == NULL) {
+            return 0;
+        }
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == NULL) {
+            return 0;
+        }
+    } while (fast != slow);
+    return 1;
 }
