bf99714fc4f5a84059434bffe24fd21aeb366dee
LeoLu <fdulyz@sina.com>
2024-03-08 20:36:01 +0800

    Lab02: Finished!

--- lab/lab02/lfsr.c
+++ lab/lab02/lfsr.c
@@ -5,6 +5,13 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint32_t position_0, position_2, position_3, position_5, left;
+    position_0 = *reg & 1;
+    position_2 = (*reg & (1 << 2)) >> 2;
+    position_3 = (*reg & (1 << 3)) >> 3;
+    position_5 = (*reg & (1 << 5)) >> 5;
+    left = position_0 ^ position_2 ^ position_3 ^ position_5;
+
+    *reg = (*reg >> 1) | (left << 15);
 }
 
