b1b41cb00568bfd357b3ad0c89748b34407a0f44
LeoLu <fdulyz@sina.com>
2024-03-11 12:09:08 +0800

    Lab04: Finish Exercise 1

--- lab/lab04/megalistmanips.s
+++ lab/lab04/megalistmanips.s
@@ -57,6 +57,7 @@ map:
     add s1, a1, x0      # save address of function in s1
     add t0, x0, x0      # t0 is a counter
 
+    jal mapLoop
     # remember that each node is 12 bytes long:
     # - 4 for the array pointer
     # - 4 for the size of the array
@@ -66,20 +67,40 @@ map:
     # are modified by the callees, even when we know the content inside the functions 
     # we call. this is to enforce the abstraction barrier of calling convention.
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
+    lw t1, 0(s0)     # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
+    li t3, 4
+    mul t4, t0, t3
+    add t1, t1, t4      # offset the array address by the count
     lw a0, 0(t1)        # load the value at that address into a0
 
+    # prologue
+    addi sp, sp, -24
+    sw ra, 0(sp)
+    sw t0, 4(sp)
+    sw t1, 8(sp)
+    sw t2, 12(sp)
+    sw s0, 16(sp)
+    sw s1, 20(sp)
+
     jalr s1             # call the function on that value.
+
+    # epilogue
+    lw ra, 0(sp)
+    lw t0, 4(sp)
+    lw t1, 8(sp)
+    lw t2, 12(sp)
+    lw s0, 16(sp)
+    lw s1, 20(sp)
+    addi sp, sp, 24
 
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    add a1, s1, x0        # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
